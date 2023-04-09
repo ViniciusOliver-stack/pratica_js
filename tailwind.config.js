8aa9130dc83270707a1ad912ec33016ca9cb7653
viniciusoliver-stack <vinicius.so.contato@hotmail.com>
2023-04-09 17:19:50 -0300

    :wrench: chore: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,19 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+      colors: {
+        dark: {
+          900: '#121212'
+        },
+        primary: {
+          900: '#FCD50C'
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
