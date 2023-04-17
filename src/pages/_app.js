a50b3102cd1f765a8dab71b9d3e3902b129eb5dd
viniciusoliver-stack <vinicius.so.contato@hotmail.com>
2023-04-16 22:48:56 -0300

    :lipstick: feat: import from nextui-org

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
+import { NextUIProvider } from '@nextui-org/react'
+
 import '@/styles/globals.css'
 import Navbar from '@/components/Navbar'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Navbar />
-      <Component {...pageProps} />
+      <NextUIProvider>
+        <Navbar />
+        <Component {...pageProps} />
+      </NextUIProvider>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,32 @@ import Header from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const post = [
+  { title: 'PomoFocus', excerpt: 'Aqui vem a descrição do projeto' },
+  {
+    title: 'CountDown',
+    excerpt: 'Aqui vem a descrição do projeto falando como funciona'
+  }
+]
+
 export default function Home() {
   return (
-    <div>
+    <div className={inter.className}>
       <Head>
         <title>Prática JS</title>
         <link rel="icon" href="/public/favicon.ico" />
       </Head>
 
       <Header />
+
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        {post.map((post, index) => (
+          <div key={post.title}>
+            {post.title}
+            {post.excerpt}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
